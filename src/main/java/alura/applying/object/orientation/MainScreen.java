61da55295eac78c2bdd7583f56269730ea26dd2c
Jean Paul <jeanpaulwebb@gmail.com>
2024-01-14 00:01:00 -0300

    :sparkles: Added class for music

---
+++ src/main/java/alura/applying/object/orientation/MainScreen.java
@@ -0,0 +1,16 @@
+package alura.applying.object.orientation;
+
+public class MainScreen {
+    public static void main(String[] args) {
+        Music myMusic = new Music();
+        myMusic.title = "Nome da Música";
+        myMusic.artist = "Nome do Artista";
+        myMusic.yearLaunch = 2023;
+        myMusic.displaysTechnicalSheet();
+        myMusic.evaluate(4.5);
+        myMusic.evaluate(3.8);
+        myMusic.evaluate(5.0);
+        double mediaReviews = myMusic.pegaMedia();
+        System.out.println("Média das avaliações: " + mediaReviews);
+    }
+}
---
+++ src/main/java/alura/applying/object/orientation/Music.java
@@ -0,0 +1,24 @@
+package alura.applying.object.orientation;
+
+public class Music {
+    String title;
+    String artist;
+    int yearLaunch;
+    double sumDasEvaluations;
+    int numAvaluations;
+
+    void displaysTechnicalSheet() {
+        System.out.println("Título da música: " + title);
+        System.out.println("Artista: " + artist);
+        System.out.println("Ano de lançamento: " + yearLaunch);
+    }
+
+    void evaluate(double nota) {
+        sumDasEvaluations += nota;
+        numAvaluations++;
+    }
+
+    double pegaMedia() {
+        return sumDasEvaluations / numAvaluations;
+    }
+}
